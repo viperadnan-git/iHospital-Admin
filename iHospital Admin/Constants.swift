e6bd37a1200f2b33fc1d76aec6bbce5aad0d03a4
viperadnan-git <viperadnan@gmail.com>
2024-07-19 02:51:52 +0530

    added local database keys

--- iHospital Admin/Constants.swift
+++ iHospital Admin/Constants.swift
@@ -12,6 +12,9 @@ struct Constants {
     static let supabaseURL = "https://abnyxrflmgrmvelohwpu.supabase.co"
     static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFibnl4cmZsbWdybXZlbG9od3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NjE0MDIsImV4cCI6MjAzNTAzNzQwMn0.1kh8Ml9krT1fj821xNHS5hP2keSaPN9hYxUFOo0yPR4"
     
+    static let localSupabaseURL = "http://172.20.10.3:8000"
+    static let localSupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzIxMjQxMDAwLAogICJleHAiOiAxODc5MDA3NDAwCn0.Q-pfkuBPpR51hqSnbOzXySLszv6uqP3gYk1klkx3_MA"
+    
     
     // MARK: - User Defaults Keys
     static let userInfoKey = "User"
